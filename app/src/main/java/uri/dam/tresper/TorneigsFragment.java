b5f039615735aa1d3e34220fe51a10ed92ed7578
uriloop <orlopez@elpuig.xeill.net>
2022-02-22 23:19:26 +0100

    Recycler preparat

--- app/src/main/java/uri/dam/tresper/TorneigsFragment.java
+++ app/src/main/java/uri/dam/tresper/TorneigsFragment.java
@@ -1,5 +1,6 @@
 package uri.dam.tresper;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -8,19 +9,57 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
 import uri.dam.tresper.databinding.FragmentTorneigsBinding;
+import uri.dam.tresper.torneigs.TorneigElement;
+import uri.dam.tresper.torneigs.TorneigsViewModel;
 
 public class TorneigsFragment extends Fragment {
 
     NavController navController;
 
     private FragmentTorneigsBinding binding;
+    TorneigsViewModel torneigsViewModel;
+    RecyclerView recyclerView;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        recyclerView = binding.recyclerView;
+
+
+        TorneigsAdapter torneigsAdapter = new TorneigsAdapter();
+        binding.recyclerView.setAdapter(torneigsAdapter);
+        /*CardSliderLayoutManager csLayoutManager=new CardSliderLayoutManager(getContext());
+        recyclerView.setLayoutManager(csLayoutManager);
+        new CardSnapHelper().attachToRecyclerView(recyclerView);*/
+
+
+
+        torneigsViewModel = new ViewModelProvider(requireActivity()).get(TorneigsViewModel.class);
+        navController = Navigation.findNavController(view);
+
+        torneigsViewModel.obtener().observe(getViewLifecycleOwner(), new Observer<List<TorneigElement>>() {
+            @Override
+            public void onChanged(List<TorneigElement> torneigElementList) {
+                torneigsAdapter.establecerLista(torneigElementList);
+            }
+        });
+
+    }
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -43,4 +82,71 @@ public class TorneigsFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+
+    class TorneigViewHolder extends RecyclerView.ViewHolder {
+        private final uri.dam.tresper.databinding.ViewholderTorneigBinding binding;
+
+        public TorneigViewHolder(uri.dam.tresper.databinding.ViewholderTorneigBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+        }
+    }
+
+
+    class TorneigsAdapter extends RecyclerView.Adapter<TorneigViewHolder> {
+
+        List<TorneigElement> torneigElementList;
+
+        @NonNull
+        @Override
+        public TorneigViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            return new TorneigViewHolder(uri.dam.tresper.databinding.ViewholderTorneigBinding.inflate(getLayoutInflater(), parent, false));
+        }
+
+        public TorneigElement obtenirTorneig(int posicion){
+            return torneigElementList.get(posicion);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull TorneigViewHolder holder, int position) {
+
+            TorneigElement torneigElement = torneigElementList.get(position);
+
+            holder.binding.nomTorneigView.setText(torneigElement.getNomTorneig());
+            holder.binding.dataView.setText(torneigElement.getDiaIhora());
+            Glide.with(getContext()).load(torneigElement.getImatgeCartell())
+                    .centerInside()
+                    .into(holder.binding.cartellView);
+            /*
+            holder.binding.cartellView.load(torneigElement.getDiaIhora());
+*/
+            //Per anar a l'item
+/*
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    torneigsViewModel.seleccionar(torneigElement);
+                    new Handler().postDelayed(new Runnable() {   // delay per a simular la carrega i que es vegi la progressBar
+                        @Override
+                        public void run() {
+                            getActivity().finish();
+                            startActivity(new Intent(getActivity(), AppActivity.class));
+                        }
+                    }, 1700);
+                }
+            });*/
+        }
+
+        @Override
+        public int getItemCount() {
+            return torneigElementList != null ? torneigElementList.size() : 0;
+        }
+
+        public void establecerLista(List<TorneigElement> torneigElementList){
+            this.torneigElementList = torneigElementList;
+            notifyDataSetChanged();
+        }
+    }
+
 }
---
+++ app/src/main/java/uri/dam/tresper/torneigs/TorneigsViewModel.java
@@ -0,0 +1,64 @@
+package uri.dam.tresper.torneigs;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
+
+import java.util.List;
+
+public class TorneigsViewModel extends AndroidViewModel {
+
+    RepositoriTorneigs repositoriTorneigs;
+
+    MutableLiveData<List<TorneigElement>> listTorneigsMutableLiveData = new MutableLiveData<>();
+    MutableLiveData<TorneigElement> torneigSeleccionat = new MutableLiveData<>();
+
+    public TorneigsViewModel(@NonNull Application application) {
+        super(application);
+        repositoriTorneigs = new RepositoriTorneigs();
+
+        listTorneigsMutableLiveData.setValue(repositoriTorneigs.obtener());
+    }
+
+    public MutableLiveData<List<TorneigElement>> obtener() {
+        return listTorneigsMutableLiveData;
+    }
+
+
+    public void seleccionar(TorneigElement torneigElement) {
+        torneigSeleccionat.setValue(torneigElement);
+    }
+
+    MutableLiveData<TorneigElement> seleccionat() {
+        return torneigSeleccionat;
+    }
+
+    /*void insertar(TorneigElement torneigElement){
+        repositoriTorneigs.insertar(torneigElement, new RepositoriTorneigs.Callback() {
+            @Override
+            public void cuandoFinalice(List<TorneigElement> torneigsList) {
+                listTorneigsMutableLiveData.setValue(torneigsList);
+            }
+        });
+    }*/
+
+   /* void eliminar(TorneigElement torneigElement){
+        repositoriTorneigs.eliminar(torneigElement, new RepositoriTorneigs.Callback() {
+            @Override
+            public void cuandoFinalice(List<TorneigElement> torneigsList) {
+                listTorneigsMutableLiveData.setValue(torneigsList);
+            }
+        });
+    }
+*/
+/*    void actualizar(TorneigElement torneigElement, float valoracion){
+        repositoriTorneigs.actualizar(torneigElement, valoracion, new RepositoriTorneigs.Callback() {
+            @Override
+            public void cuandoFinalice(List<TorneigElement> torneigsList) {
+                listTorneigsMutableLiveData.setValue(torneigsList);
+            }
+        });
+    }*/
+}
